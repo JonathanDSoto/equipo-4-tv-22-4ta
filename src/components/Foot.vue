f02f41ec9430110502023146a3f12cf1f18184cd
JoseArmandoTG20 <josearman2_19@alu.uabcs.mx>
2022-10-28 16:54:53 -0600

    cambiando foter

--- src/components/Foot.vue
+++ src/components/Foot.vue
@@ -1,16 +1,15 @@
 
-<script>Document.write(new Date().getFullYear())</script>
 <template>
   <div class="container-fluid">
-        <div class="row">
-            <div class="col-sm-6">
-                2022 © Velzon.
-            </div>
-            <div class="col-sm-6">
-                <div class="text-sm-end d-none d-sm-block">
-                    Design & Develop by Themesbrand
-                </div>
-            </div>
+    <div class="row">
+      <div class="col-sm-6">
+        {{new Date().getFullYear()}} © Velzon.
+      </div>
+      <div class="col-sm-6">
+        <div class="text-sm-end d-none d-sm-block">
+          Design & Develop by Themesbrand
         </div>
+      </div>
     </div>
+  </div>
 </template>
